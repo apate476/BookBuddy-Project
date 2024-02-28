b6169f57e47ec2d292681387971be2d635bf736e
Arya Patel <arya@MacBook-Pro-84.local>
2024-02-28 13:56:08 -0600

    book list and details finished

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,1 +1,46 @@
 /* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function Books() {
+  const [allBooks, setAllBooks] = useState([]);
+  const [searchBook, setSearchBook] = useState("");
+
+  useEffect(() => {
+    async function getBooks() {
+      try {
+        const response = await axios.get(
+          "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books"
+        );
+        const books = response.data.books; // Access the books from response.data
+        // console.log("books", books);
+        setAllBooks(books);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getBooks();
+  }, []);
+
+  return (
+    <main>
+      <h2 className="book-header">Book List</h2>
+      <div className="book-container">
+        {allBooks.map((singleBook) => (
+          <div className="book-card" key={singleBook.id}>
+            <div className="book-page">
+              <h3>Title: {singleBook.title}</h3>
+              <h4>Author: {singleBook.author}</h4>
+              <img src={singleBook.coverimage} alt={singleBook.title} />
+              <Link to={`/books/${singleBook.id}`}>View Details</Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
+
+export default Books;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,1 +1,47 @@
 /* TODO - add your code to create a functional React component that renders details for a single book. Fetch the book data from the provided API. You may consider conditionally rendering a 'Checkout' button for logged in users. */
+
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+function SingleBook() {
+  const { bookId } = useParams();
+  const [bookDetails, setBookDetails] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function getBookDetails() {
+      console.log(bookId);
+      try {
+        const response = await axios.get(
+          `https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books/${bookId}`
+        );
+        console.log(response);
+        setBookDetails(response.data.book);
+      } catch (error) {
+        console.error(error.response);
+      }
+    }
+    getBookDetails();
+  }, [bookId]);
+
+  if (!bookDetails) {
+    return <p>Loading Book Details...</p>;
+  }
+
+  return (
+    <div className="book-details-container">
+      <h2>Book Details</h2>
+      <img src={bookDetails.coverimage} />
+      <div>
+        <h3>Title: {bookDetails.title}</h3>
+        <h4>Author: {bookDetails.author}</h4>
+        <p>Descripition: {bookDetails.description}</p>
+        <p>Availability: {bookDetails.available ? "Yes" : "No"}</p>
+      </div>
+      <button onClick={() => navigate("/books")}>Back to the List</button>
+    </div>
+  );
+}
+
+export default SingleBook;
